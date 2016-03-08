8a7b0c16f84094c5100f5201eac7ca133209e169
scarviz <scarviz@gmail.com>
2016-03-08 23:36:19 +0900

    to enable to move the tank, and to improve the appearance of tank

--- Assets/Scripts/TankMove.cs
+++ Assets/Scripts/TankMove.cs
@@ -2,14 +2,38 @@
 using System.Collections;
 
 public class TankMove : MonoBehaviour {
+	/// <summary>移動する速さ</summary>
+	public float mSpeed = 10f;
+	
+	private CharacterController mCharCtrl;
 
 	// Use this for initialization
 	void Start () {
-	
+		// Tank(このスクリプトが設定されているGameObject)に追加したCharacterControllerをここで取得する
+		mCharCtrl = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		// 移動する方向(x軸方向へ前進後進)
+		// ※ Verticalはキー入力の上下
+		var moveDirection = new Vector3(Input.GetAxis("Vertical"), 0, 0);
+
+		// Tankの現在の座標に移動する方向を追加する
+		// ※ transformはこのスクリプトが設定されているGameObjectの方向や座標情報などを持っている
+		moveDirection = transform.TransformDirection(moveDirection);
+
+		// 移動する速さ(移動量)を加味する
+		moveDirection *= mSpeed;
+
+		// このUpdate処理(1フレーム当たり)の移動量からTankを移動させる
+		// ※ Time.deltaTimeは前回Updateが呼ばれた時からの経過時間が格納される
+		//    フレーム落ちなどでスキップされた分を考慮して移動量を算出する必要があるため
+		mCharCtrl.Move(moveDirection * Time.deltaTime);
+
+		// 回転(y軸中心に回転)
+		// ※ Horizontalはキー入力の左右
+		transform.Rotate(0, Input.GetAxis("Horizontal") * mSpeed, 0);
 	}
 }
