5ecd793ecc633a2f0f4c14d059ee74008349cd84
scarviz <scarviz@gmail.com>
2016-03-09 13:01:10 +0900

    add shot interval

--- Assets/Scripts/TankShot.cs
+++ Assets/Scripts/TankShot.cs
@@ -9,6 +9,12 @@ public class TankShot : MonoBehaviour
 	/// <summary>砲塔</summary>
 	public GameObject cylinder;
 
+	/// <summary>発射間隔</summary>
+	public float shotInterval = 1.5f;
+
+	/// <summary>発射間隔の積み上げ値</summary>
+	private float stackShotInterval = 0f;
+
 	// Use this for initialization
 	void Start()
 	{
@@ -17,10 +23,23 @@ public class TankShot : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if (Input.GetButton("Fire1"))
+		stackShotInterval += Time.deltaTime;
+
+		if (Input.GetButton("Fire1") && EnableShot())
 		{
 			// 砲弾を生成する(このスクリプトが設定されているGameObjectの位置と向きを合わせる)
 			Instantiate(bullet, cylinder.transform.position, transform.rotation);
+
+			stackShotInterval = 0f;
 		}
+	}
+
+	/// <summary>
+	/// 発射可能かどうか
+	/// </summary>
+	/// <returns>可否</returns>
+	private bool EnableShot()
+	{
+		return shotInterval <= stackShotInterval;
 	}
 }
