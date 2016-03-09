afc2c7518ba7eee9d579ea711e6b746ad9be6140
scarviz <scarviz@gmail.com>
2016-03-09 13:43:40 +0900

    add bullet destroy event

--- Assets/Scripts/BulletShoot.cs
+++ Assets/Scripts/BulletShoot.cs
@@ -6,8 +6,10 @@ public class BulletShoot : MonoBehaviour {
 	public float speed = 100f;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		// 1.5秒後に自動消滅
+		Destroy(gameObject, 1.5f);
 	}
 	
 	// Update is called once per frame
@@ -15,5 +17,15 @@ public class BulletShoot : MonoBehaviour {
 		// 前方に進める
 		// ※ x軸が前方なのでrightを使用している。z軸方向はforward、y軸方向はupになる。
 		transform.position += transform.right * Time.deltaTime * speed;
+	}
+
+	/// <summary>
+	/// 衝突時イベント
+	/// </summary>
+	/// <param name="collision">衝突対象</param>
+	void OnCollisionEnter(Collision collision)
+	{
+		// 衝突時に消滅
+		Destroy(gameObject);
 	}
 }
