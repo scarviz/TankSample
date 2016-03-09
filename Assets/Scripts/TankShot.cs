3a91e0207d5d30ee6044d6d2887ba5bee954ad7a
scarviz <scarviz@gmail.com>
2016-03-09 09:43:24 +0900

    shoot to cylinder

--- Assets/Scripts/TankShot.cs
+++ Assets/Scripts/TankShot.cs
@@ -1,20 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TankShot : MonoBehaviour {
+public class TankShot : MonoBehaviour
+{
 	/// <summary>砲弾</summary>
 	public GameObject bullet;
 
+	/// <summary>砲塔</summary>
+	private GameObject cylinder;
+
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
+		cylinder = gameObject.transform.FindChild("GunTurret/Cylinder").gameObject;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		if (Input.GetButton("Fire1"))
 		{
 			// 砲弾を生成する(このスクリプトが設定されているGameObjectの位置と向きを合わせる)
-			Instantiate(bullet, transform.position, transform.rotation);
+			Instantiate(bullet, cylinder.transform.position, transform.rotation);
 		}
 	}
 }
